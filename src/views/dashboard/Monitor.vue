170b4838bf1fab8baa0e50a8de90667a648a8a41
dd <hollykunge@163.com>
2019-04-13 14:11:50 +0800

    完美的工作台

--- src/views/dashboard/Monitor.vue
+++ src/views/dashboard/Monitor.vue
@@ -1,123 +1,65 @@
 <template>
-  <div>
+  <div style="margin-bottom: 64px">
     <div class="antd-pro-pages-dashboard-analysis-twoColLayout" :class="isDesktop() ? 'desktop' : ''">
-      <Container @drop="onDrop">
-        <a-row :gutter="8">
-          <a-col :xl="12" :lg="12" :md="12" :sm="24" :xs="24">
+      <grid-layout
+        :layout.sync="layout"
+        :col-num="12"
+        :row-height="52"
+        :max-rows="12"
+        :is-draggable="true"
+        :is-resizable="true"
+        :is-mirrored="false"
+        :vertical-compact="true"
+        :margin="[10, 10]"
+        :use-css-transforms="true"
+      >
+        <grid-item
+          v-for="grid in layout"
+          dragIgnoreFrom=".card-content"
+          :minH="cardSize.minH"
+          :maxH="cardSize.maxH"
+          :minW="cardSize.minW"
+          :key="grid.id"
+          :x="grid.x"
+          :y="grid.y"
+          :w="grid.w"
+          :h="grid.h"
+          :i="grid.i">
+          <a-card
+            :headStyle="headStyle"
+            :loading="loading"
+            :bordered="true"
+            :title="grid.title"
+            :style="{ minHeight: '300px' }">
+            <a-popover
+              placement="left"
+              slot="extra"
+              trigger="click">
+              <template slot="content">
+                <a>移除卡片</a>
+              </template>
+              <a href="#"><a-icon type="tool" /></a>
+            </a-popover>
 
-            <Draggable>
-              <a-card
-                :headStyle="headStyle"
-                :loading="loading"
-                :bordered="true"
-                title="研讨消息"
-                :style="{ marginTop: '6px', minHeight: '296px' }">
-                <a href="#" slot="extra"><a-icon type="tool" /></a>
-                <div v-if="talkData.length!=0">
-                  <a-list
-                    bordered
-                    :dataSource="talkData"
-                  >
-                    <a-list-item slot="renderItem" slot-scope="item">{{ item }}</a-list-item>
-                    <div slot="header">Header</div>
-                    <div slot="footer">Footer</div>
-                  </a-list>
-                </div>
-                <div v-else style="margin: 40px auto 0 auto;text-align: center;">
-                  <a-icon type="file-exclamation" theme="twoTone" :style="fontSize" />
-                  <p class="description">卡片暂无内容</p>
-                </div>
-              </a-card>
-            </draggable>
-          </a-col>
-          <a-col :xl="12" :lg="12" :md="12" :sm="24" :xs="24">
-            <a-card
-              :headStyle="headStyle"
-              class="antd-pro-pages-dashboard-analysis-salesCard"
-              :loading="loading"
-              :bordered="true"
-              title="待办事项"
-              :style="{ marginTop: '6px', minHeight: '296px' }">
-              <a href="#" slot="extra"><a-icon type="tool" /></a>
-              <div v-if="talkData.length!=0">
-                <a-list
-                  bordered
-                  :dataSource="talkData"
-                >
-                  <a-list-item slot="renderItem" slot-scope="item">{{ item }}</a-list-item>
-                  <div slot="header">Header</div>
-                  <div slot="footer">Footer</div>
-                </a-list>
-              </div>
-              <div v-else style="margin: 40px auto 0 auto;text-align: center;">
-                <a-icon type="file-exclamation" theme="twoTone" :style="fontSize" />
-                <p class="description">卡片暂无内容</p>
-              </div>
-            </a-card>
-          </a-col>
-          <a-col :xl="12" :lg="12" :md="12" :sm="24" :xs="24">
-            <a-card
-              :headStyle="headStyle"
-              :loading="loading"
-              :bordered="true"
-              title="会议室"
-              :style="{ marginTop: '6px', minHeight: '296px' }">
-              <a href="#" slot="extra"><a-icon type="tool" /></a>
-              <div v-if="talkData.length!=0">
-                <a-list
-                  bordered
-                  :dataSource="talkData"
-                >
-                  <a-list-item slot="renderItem" slot-scope="item">{{ item }}</a-list-item>
-                  <div slot="header">Header</div>
-                  <div slot="footer">Footer</div>
-                </a-list>
-              </div>
-              <div v-else style="margin: 40px auto 0 auto;text-align: center;">
-                <a-icon type="file-exclamation" theme="twoTone" :style="fontSize" />
-                <p class="description">卡片暂无内容</p>
-              </div>
-            </a-card>
-          </a-col>
-          <a-col :xl="12" :lg="12" :md="12" :sm="24" :xs="24">
-            <a-card
-              :headStyle="headStyle"
-              class="antd-pro-pages-dashboard-analysis-salesCard"
-              :loading="loading"
-              :bordered="true"
-              title="资源池"
-              :style="{ marginTop: '6px', minHeight: '296px' }">
-              <a href="#" slot="extra"><a-icon type="tool" /></a>
-              <div v-if="talkData.length!=0">
-                <a-list
-                  bordered
-                  :dataSource="talkData"
-                >
-                  <a-list-item slot="renderItem" slot-scope="item">{{ item }}</a-list-item>
-                  <div slot="header">Header</div>
-                  <div slot="footer">Footer</div>
-                </a-list>
-              </div>
-              <div v-else style="margin: 40px auto 0 auto;text-align: center;">
-                <a-icon type="file-exclamation" theme="twoTone" :style="fontSize" />
-                <p class="description">卡片暂无内容</p>
-              </div>
-            </a-card>
-          </a-col>
-        </a-row>
-      </container>
-      <!-- <a-row :gutter="8">
-      </a-row> -->
-      <!-- <div class="simple-page">
-        <Container @drop="onDrop">
-          <Draggable v-for="item in items" :key="item.id">
-            <div class="draggable-item">
-              {{ item.data }}
+            <div v-if="talkData.length!=0" class="card-content">
+              <a-list
+                bordered
+                :dataSource="talkData"
+              >
+                <a-list-item slot="renderItem" slot-scope="item">{{ item }}</a-list-item>
+                <div slot="header">Header</div>
+                <div slot="footer">Footer</div>
+              </a-list>
             </div>
-          </Draggable>
-        </Container>
-      </div> -->
+            <div v-else style="margin: 40px auto 0 auto;text-align: center;" class="card-content">
+              <a-icon type="file-exclamation" theme="twoTone" :style="fontSize" />
+              <p class="description">卡片暂无内容</p>
+            </div>
+          </a-card>
+        </grid-item>
+      </grid-layout>
     </div>
+    <!--这个地方放置最近访问-->
     <footer-tool-bar :style="{height:'72px', width: isSideMenu() && isDesktop() ? `calc(100% - ${sidebarOpened ? 256 : 80}px)` : '100%'}">
 
     </footer-tool-bar>
@@ -127,12 +69,23 @@
 <script>
 import { mixin, mixinDevice } from '@/utils/mixin'
 import FooterToolBar from '@/components/FooterToolbar'
-import { Container, Draggable } from 'vue-smooth-dnd'
-import { applyDrag, generateItems } from './utils'
+// import { Container, Draggable } from 'vue-smooth-dnd'
+// import { applyDrag, generateItems } from './utils'
+import VueGridLayout from 'vue-grid-layout'
 
 const talkData = [
 ]
-
+// 工作台看板模拟数据
+var layoutCards = [
+  { 'x': 0, 'y': 0, 'w': 6, 'h': 5, 'i': '0', 'title': '研讨信息' },
+  { 'x': 6, 'y': 0, 'w': 6, 'h': 5, 'i': '1', 'title': '待办事项' },
+  { 'x': 0, 'y': 5, 'w': 6, 'h': 5, 'i': '2', 'title': '我的日程' },
+  { 'x': 6, 'y': 5, 'w': 6, 'h': 5, 'i': '3', 'title': '我的资源' },
+  { 'x': 0, 'y': 5, 'w': 6, 'h': 5, 'i': '2', 'title': '我的日程' },
+  { 'x': 6, 'y': 5, 'w': 6, 'h': 5, 'i': '3', 'title': '我的资源' },
+  { 'x': 0, 'y': 5, 'w': 6, 'h': 5, 'i': '2', 'title': '我的日程' },
+  { 'x': 6, 'y': 5, 'w': 6, 'h': 5, 'i': '3', 'title': '我的资源' }
+]
 export default {
   name: 'Monitor',
   mixins: [mixin, mixinDevice],
@@ -143,13 +96,17 @@ export default {
       fontSize: { fontSize: '52px' },
       talkData: talkData,
       visible: false,
-      items: generateItems(50, i => ({ id: i, data: 'Draggable' + i }))
+      layout: layoutCards,
+      cardSize: { maxH: 5, minH: 5, maxW: 12, minW: 3 }
+      // items: generateItems(50, i => ({ id: i, data: 'Draggable' + i }))
     }
   },
   components: {
     FooterToolBar,
-    Container,
-    Draggable
+    // Container,
+    // Draggable,
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem
   },
   created () {
     setTimeout(() => {
@@ -157,9 +114,9 @@ export default {
     }, 1000)
   },
   methods: {
-    onDrop (dropResult) {
-      this.items = applyDrag(this.items, dropResult)
-    }
+    // onDrop (dropResult) {
+    //   this.items = applyDrag(this.items, dropResult)
+    // }
   }
 }
 </script>
