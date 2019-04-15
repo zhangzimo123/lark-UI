a1166c87852ccd3f353f3947230c7bf120262ee4
jihainan <alwaysbegoing@163.com>
2019-04-15 09:59:46 +0800

    增加是否最大化的判断

--- src/utils/client.js
+++ src/utils/client.js
@@ -6,17 +6,38 @@
  * // NOTE: 添加eslint-disable避免编译报错
  */
 
-// 最小化窗口
+/**
+ * 最小化窗口
+ */
 export function minimizeWindow () {
   console.log('调用客户端**最小化窗口**的方法')
 }
 
-// 最大/小化切换
+/**
+ * 最大/小化切换
+ */
 export function switchWindow () {
   console.log('调用客户端**切换窗口**的方法')
 }
 
-// 关闭窗口
+/**
+ * 关闭窗口
+ */
 export function closeWindow () {
   console.log('调用客户端**关闭窗口**的方法')
+}
+
+/**
+ * 判断是否最大化
+ * 该方法仅在chrome浏览器上有效
+ * @return Boolean
+ */
+export function isFullScreen () {
+  let explorer = window.navigator.userAgent.toLowerCase()
+  if (explorer.indexOf(chrome) > 0) {
+    return window.outerHeight === screen.availHeight && window.outerWidth === screen.availWidth
+  } else {
+    // 非chrome浏览器，待处理
+    return false
+  }
 }
