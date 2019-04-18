03d40c45f0f8890d9335a042deb85b8e87abb204
jihainan <alwaysbegoing@163.com>
2019-04-18 23:47:32 +0800

    新增群组信息展示组件

---
+++ src/components/Talk/GroupInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- group info component -->
+  <div class="group-info">
+
+    <div v-if="Object.keys(selected).length == 0" class="unselected-tip">
+      <h1>未选中群组</h1>
+    </div>
+
+    <div class="selected-info">
+      <h1>群组信息</h1>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GroupInfo',
+  props: {
+    // the selected group
+    selected: {
+      type: Object,
+      default: () => ({}),
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
