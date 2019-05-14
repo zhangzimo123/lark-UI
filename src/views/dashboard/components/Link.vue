50cff73614ad1a944bf31546802c2242254f1384
xueyc@mksoftapp.com <xueyc@mksoftapp.com>
2019-05-14 20:17:00 +0800

    常用链接

--- src/views/dashboard/components/Link.vue
+++ src/views/dashboard/components/Link.vue
@@ -1,17 +1,22 @@
 <template>
-  <a-row :gutter="5">
-    <a-col :span="2" v-for="(item,index) in data.content" :key="'link-'+index">
-      <a :href="item.link" target="_blank">
-        <a-card hoverable class="link-ant-card">
-          <img class="link-ant-card-img" slot="cover" :title="item.name" :src="item.img"/>
-          <span>{{ item.name }}</span>
+  <div class="link-ant-row">
+    <div v-for="(item,index) in data.content" :key="'link-'+index" class="link-ant-item">
+      <img class="link-ant-card-img" :src="item.img"/>
+    </div>
+  </div>
+  <!--<a-row :gutter="5" class="link-ant-row">-->
+    <!--<a-col :span="2" v-for="(item,index) in data.content" :key="'link-'+index">-->
+      <!--<a :href="item.link" target="_blank">-->
+        <!--<a-card hoverable class="link-ant-card">-->
+          <!--<img class="link-ant-card-img" slot="cover" :title="item.name" :src="item.img"/>-->
+          <!--<span>{{ item.name }}</span>-->
         <!-- <a-card-meta :title="item.name" :description="item.description" style="backgrounp:blue;"> -->
         <!-- <template slot="description">{{ item.description }}</template> -->
-        <!-- </a-card-meta> -->
-        </a-card>
-      </a>
-    </a-col>
-  </a-row>
+        <!--&lt;!&ndash; </a-card-meta> &ndash;&gt;-->
+        <!--</a-card>-->
+      <!--</a>-->
+    <!--</a-col>-->
+  <!--</a-row>-->
 </template>
 <script>
 export default {
@@ -35,6 +40,25 @@ div.ant-card-body{
   font-weight: 600;
 }
 .link-ant-card-img{
-  height: 48px;
+  /*height: 48px;*/
+  /*float: left;*/
+  /*height: 70px;*/
+  width: 70px;
+
+}
+.link-ant-row{
+  /*white-space:nowrap;*/
+  /*overflow-x:hidden;*/
+  /*overflow:auto;*/
+  width: 100%;
+  /*overflow-x:scroll;*/
+  overflow-y:scroll;
+  height: 100px;
+  /*background-color: blue;*/
+  padding-bottom: 28px;
+}
+.link-ant-item{
+  white-space:nowrap;
+  display: inline-block;
 }
 </style>
