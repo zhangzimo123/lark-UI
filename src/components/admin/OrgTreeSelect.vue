69949a5d2a0199c8e672abf801c2f2f4a07e0f03
qiaocao <50604364+qiaocao@users.noreply.github.com>
2019-05-28 16:23:52 +0800

    树选择器和多选下拉框组件调整

--- src/components/admin/OrgTreeSelect.vue
+++ src/components/admin/OrgTreeSelect.vue
@@ -1,47 +1,66 @@
 <template>
-    <div>
-        <a-tree-select
-            showSearch
-            :dropdownStyle="{ maxHeight: '200px', overflow: 'auto' }"
-            :treeData="orgTree"
-            treeDefaultExpandAll
-            allowClear
-            :value="inputvalue"
-            @change="onChange"
-        ></a-tree-select>
-    </div>
+  <div>
+    <a-tree-select
+      :dropdownStyle="{ maxHeight: '200px', overflow: 'auto' }"
+      :treeData="orgTree"
+      treeDefaultExpandAll
+      allowClear
+      :value="orgid"
+      @change="onChange"
+      style="width:100%"
+    ></a-tree-select>
+  </div>
 </template>
 <script>
 import { getOrgTree } from '@/api/admin'
 export default {
-    name: 'OrgTreeSelect',
-    components: {
-        getOrgTree
-    },
-    data () {
-        return {
-            orgTree: [],
-            inputvalue: this.pvalue
-        }
-    },
-    created () {
-        // 获取树形组织信息
-        getOrgTree().then(res => {
-            this.orgTree = res.result
-        })
-    },
-    props: {
-        pvalue : String
-    },
-    methods: {
-        /**
-         * change事件
-         */
-        onChange (value) {
-            this.$emit('change', value)
-            this.inputvalue = value
-        }
+  name: 'OrgTreeSelect',
+  components: {
+    getOrgTree
+  },
+  data () {
+    return {
+      orgTree: [],
+      orgid: ''
     }
+  },
+  created () {
+    if (this.dataSource.length === 0) {
+      // 获取树形组织信息
+      getOrgTree().then(res => {
+        this.orgTree = res.result
+      })
+    } else {
+      this.orgTree = dataSource
+    }
+  },
+  props: {
+    // 树选择器选中内容 非必填可传递
+    values: {
+      type: String,
+      default: ''
+    },
+    // 树选择器下拉框内容 非必填可传递
+    dataSource: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  },
+  watch: {
+    values (val) {
+      this.orgid = val
+    }
+  },
+  methods: {
+    /**
+     * change事件
+     */
+    onChange (value) {
+      this.$emit('changOrg', value)
+      this.orgid = value
+    }
+  }
 }
 </script>
-
